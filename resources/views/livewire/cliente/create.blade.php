6a0a9a50947c66f202b8cf0c4442b05b33b54566
guilhermedlacerda <guilhermedossantoslacerda@gmail.com>
2025-03-25 10:39:41 -0300

    Corrijindo Cadastro e pagina de cadastro do cliente

--- resources/views/livewire/cliente/create.blade.php
+++ resources/views/livewire/cliente/create.blade.php
@@ -10,49 +10,65 @@
         
             <h3 class="card-header d-flex justify-content-center">Cadastrar-se</h3>
           
-
-
         <div class="card-body ">
             <p class="d-flex justify-content-center mb-0"><ins><strong>Cliente</strong></ins></p>
             <form wire:submit.prevent="store">
                 <div class="mb-3">
                     <label for="nome" class="form-label">Nome</label>
                     <input type="text" class="form-control border-radius: 50px; " id="nome" name="nome" placeholder="Insira seu Nome" wire:model.defer="nome">
+                    @error('nome')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="endereco">Endereco</label>
                     <input type="text" name="endereco" id="endereco" class="form-control" wire:model.defer="endereco" placeholder="Insira seu endereço">
+                    @error('endereco')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="telefone">Telefone</label>
                     <input class="form-control" name="telefone" id="telefone" wire:model.defer="telefone" placeholder="Insira seu telefone"></input>
+                    @error('telefone')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
-                    <label for="cpf">CPF</label>
-                    <input class="form-control" name="cpf" id="cpf" wire:model.defer="cpf" placeholder="Insira seu CPF"></input>
+                    <label for="cpf" class="form-label">CPF</label>
+                    <input type="text" class="form-control @error('cpf') is-invalid @enderror" 
+                           name="cpf" id="cpf" wire:model.defer="cpf" placeholder="Insira seu CPF">
+                    
+                    @error('cpf')
+                        <div class="invalid-feedback d-block">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
+                    <label for="email">Email</label>
+                    <input class="form-control" name="email" id="email" wire:model.defer="email" placeholder="Insira seu Email"></input>
+                    @error('email')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
 
                 <div class="mb-3">
                     <label for="senha">Senha</label>
                     <input type="password" class="form-control" name="senha" id="senha" wire:model.defer="senha" placeholder="Insira sua Senha"></input>
+                    @error('senha')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
                 </div>
 
                 <div class="mb-3 d-flex justify-content-center">
                     <button type="submit" class="btn btn-dark w-75 p-3">Cadastrar</button>
                 </div>
-                
-                {{-- <div class="d-flex justify-content-center">
-                    <p class="mx-1">cadastrar-se como</p>  <a href="#" target="_blank" class="text-primary ml-5">Administrador</a> <p> / </p> <a href="#" target="_blank" class="text-primary ml-5">Professor</a>
-                </div> --}}
             </form>
-
-            
         </div>
     </div>
-    
-    
-  
 </div>
